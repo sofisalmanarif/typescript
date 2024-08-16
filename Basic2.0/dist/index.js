1933b0fc34fdc25abffc9c4819870f7f160dd882
sofisalmanarif <sofisalman9906@gmail.com>
2024-08-16 21:53:30 +0530

    datatypes in ts

---
+++ Basic2.0/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let a = 12;
+a = "hello";
+a.toLowerCase;
+let check;
+let arr = [1, 2];
+arr.push(3);
+console.log(a);
---
+++ Basic2.0/src/index.ts
@@ -0,0 +1,10 @@
+let a: number | string = 12;
+a="hello"
+a.toLowerCase
+
+let check:boolean
+
+
+let arr :number [] = [1,2] ;
+arr.push(3)
+console.log(a)
