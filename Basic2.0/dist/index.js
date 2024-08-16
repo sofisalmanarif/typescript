5e49b9d0200d8ece7b8ce9968a182b496bc44761
sofisalmanarif <sofisalman9906@gmail.com>
2024-08-16 22:07:29 +0530

    touple in js

--- Basic2.0/dist/index.js
+++ Basic2.0/dist/index.js
@@ -6,3 +6,6 @@ let check;
 let arr = [1, 2];
 arr.push(3);
 console.log(a);
+let arr2 = ["salman",]; //second way of cratinf arrays with any type same as generics
+// creating touple ins ts fixed size array
+let arr3 = [1, 2, "salman"]; //create a array called touple with fixed size of 3
--- Basic2.0/src/index.ts
+++ Basic2.0/src/index.ts
@@ -8,3 +8,11 @@ let check:boolean
 let arr :number [] = [1,2] ;
 arr.push(3)
 console.log(a)
+
+
+let arr2:Array<string> = ["salman",]  //second way of cratinf arrays with any type same as generics
+
+// creating touple ins ts fixed size array
+
+
+let arr3 :[number,number,string] = [1,2,"salman"]   //create a array called touple with fixed size of 3
