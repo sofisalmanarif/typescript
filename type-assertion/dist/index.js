60fb887a11835f3c468f62424860fc2237fe623d
sofisalmanarif <sofisalman9906@gmail.com>
2024-08-17 21:10:04 +0530

    DOM operations using type assertions

--- type-assertion/dist/index.js
+++ type-assertion/dist/index.js
@@ -3,3 +3,13 @@ const btn = document.getElementById("btn"); //here we say ts that its a htmlem=l
 btn.addEventListener("click", () => console.log("clicked"));
 const img = document.getElementById("img");
 img.src = "https://picsum.photos/200/300";
+const form = document.querySelector("form");
+form.onsubmit = (e) => {
+    e.preventDefault();
+    const input = document.querySelector("form > input");
+    console.log("first", input.value);
+    const h2 = document.createElement("h2");
+    h2.innerText = input.value;
+    const body = document.querySelector("body");
+    body.appendChild(h2);
+};
--- type-assertion/src/index.ts
+++ type-assertion/src/index.ts
@@ -4,3 +4,17 @@ btn.addEventListener("click",()=>console.log("clicked"))
 
 const img =  document.getElementById("img") as HTMLImageElement
 img.src = "https://picsum.photos/200/300"
+
+
+
+const form = document.querySelector("form") as HTMLFormElement
+form.onsubmit = (e )=>{
+    e.preventDefault()
+    const input = document.querySelector("form > input") as HTMLInputElement
+    console.log("first",input.value)
+    const h2 =  document.createElement("h2") as HTMLHeadElement
+    h2.innerText = input.value
+    const body  = document.querySelector("body") as HTMLBodyElement
+
+    body.appendChild(h2)
+}
