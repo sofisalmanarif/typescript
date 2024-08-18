d02f1bf1fcf0ba6233c53702fcd070673810b926
sofisalmanarif <sofisalman9906@gmail.com>
2024-08-18 17:37:59 +0530

    children in react

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,16 +1,28 @@
 import './App.css'
 import Box from './components/Box'
 
+
+const Semobox =()=>{
+  return(
+  <div>
+    <h1>hello i am a semi box</h1>
+  </div>)
+}
 function App() {
 
 
   return (
     <>
     <div>
-      <Box name='salman' age={23} />
+      <Box name='salman' age={23} >
+        {/* <Semobox/> */}
+        </Box>
     </div>
     </>
   )
 }
 
 export default App
+
+
+
--- React/src/components/Box.tsx
+++ React/src/components/Box.tsx
@@ -1,12 +1,18 @@
+import { ReactNode } from "react";
+
 type PropType ={
     name:string;
     age:number;
+    children?:ReactNode
 }
 
-const Box = ({name,age}:PropType) => {
+const Box = ({name,age,children}:PropType) => {
   return (
     <div>{name}
     <p>{age}</p>
+    {
+        children?children:<p>i have no children</p>
+    }
     </div>
   )
 }
