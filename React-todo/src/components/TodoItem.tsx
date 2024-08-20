128a35d6608a44ace1f979842a0960256e2dae12
sofisalmanarif <sofisalman9906@gmail.com>
2024-08-20 13:06:22 +0530

    bugs fixed

--- React-todo/src/components/TodoItem.tsx
+++ React-todo/src/components/TodoItem.tsx
@@ -34,7 +34,7 @@ const TodoItem : React.FC<TodoItemProps>= ({todo}) => {
 
   return (
     <div className='flex items-center py-1 gap-2   border-b-zinc-500'>
-      <input className='rounded-full p-5 flex w-8 bg-green-400 py-10' checked ={todo.isCompleted} onChange={()=>{handleIsCompleated(todo.id,todo.title)}} type="checkbox" />
+      <input className='rounded-full p-5 flex w-8 bg-green-400 py-10' checked ={todo.isCompleted} onChange={()=>{handleIsCompleated(todo.id)}} type="checkbox" />
       <div className='w-[90%] py-1'>{todo.title} </div>
       <button onClick={()=>{deleteHandler(todo.id)}} className='px-4 py-1 bg-red-300 rounded-md'>delete</button>
     </div>
